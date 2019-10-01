8983933475fc7bc56dc59b9fb551d2c6f3605b70
ngthienan <ngthienan10032000@gmail.com>
2019-10-01 15:26:23 +0700

    Test

---
+++ ConsoleApplication3/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.Write("A[{0}] =", i);
+                string str = Console.ReadLine();
+                     int.Parse(str)
+            }
+        }
+    }
+}

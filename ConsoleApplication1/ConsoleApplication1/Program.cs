2ffc22c3fdfada11b22e92b63e1b365cf2f386b6
trminhduc <minhhducc201@gmail.com>
2019-10-01 14:34:12 +0700

    Test4

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("hello github");
+        }
+        public static bool IsOdd(int n)
+        {
+            return false;
+        }
+
+        public static bool IsEven(int n)
+        {
+            return true;
         }
     }
 }
---
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+          
+        public void TestIsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+
+        }
+        [TestMethod]
+        public void TestCeil()
+        {
+            int rs1= Program.Ceil((float)2.3);
+            Assert.AreEqual(3, rs1);
+
+            int rs2= Program.Ceil(-2.3f);
+            Assert.AreEqual(-2, rs2);
+
+           // int rs3 = Program.Ceil(3);
+            //Assert.AreEqual()
+        }
+         [TestMethod]
+        public void TestFloor()
+        {
+           int rs = Program.Floor((float)2.3);
+             Assert.AreEqual(2,rs);
+
+             int rs5 = Program.Floor(-2.3f);
+             Assert.AreEqual(-3, rs5);
+
+        }
+         [TestMethod]
+
+         public void TestRand1()
+         {
+            
+         }
+        }
+    }
+
